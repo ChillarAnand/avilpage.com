3115c366215ad3d77a5963a15c80e667b34971c4
Chillar Anand <anand21nanda@gmail.com>
2018-06-02 16:06:06 +0530

    exp: Enable disqus comments

--- conf.py
+++ conf.py
@@ -862,12 +862,12 @@ CONTENT_FOOTER_FORMATS = {
 # systems.  The following comment systems are supported by Nikola:
 #   disqus, facebook, googleplus, intensedebate, isso, livefyre, muut
 # You can leave this option blank to disable comments.
-COMMENT_SYSTEM = None
+COMMENT_SYSTEM = 'disqus'
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "nikolademo"
+COMMENT_SYSTEM_ID = "avilpage"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
