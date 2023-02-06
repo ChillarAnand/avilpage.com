11afd47b7236d6b44718a7299bf81eb5f9affa73
chillaranand <anand21nanda@gmail.com>
2023-02-06 13:31:43 +0530

    feat: added reloadium script

---
+++ scripts/dynamic.py
@@ -0,0 +1,27 @@
+from data import get_data
+
+
+def process_data(data):
+    try:
+        squares = [i ** 2 for i in data]
+        return squares
+    except Exception:
+        return []
+
+
+def fetch_data():
+    try:
+        data = get_data()
+    except Exception:
+        data = []
+    return data
+
+
+def main():
+    data = fetch_data()
+    result = process_data(data)
+    print(f'Result: {result}')
+
+
+if __name__ == '__main__':
+    main()
